bd2bf63757c8f3af61c4603d1cbeadd0f72a41bf
Nick Webb <njwebb@ucdavis.edu>
2018-03-18 21:21:53 -0700

    implemented insertion, check for overlap

--- intervaltree.go
+++ intervaltree.go
@@ -3,19 +3,43 @@ package intervaltree
 import (
     "time"
     "errors"
+    "fmt"
 )
 
 
+/* 
+    Base IntervalTree class.
+    Entry point for the tree (instead of just holding a pointer to root).
+ */
 type IntervalTree struct {
-    Root *IntervalTreeNode
+    root *IntervalTreeNode
 }
 
-func NewIntervalTree(inode *IntervalTreeNode) *IntervalTree {
-    return &IntervalTree{inode}
+func NewIntervalTree(node *IntervalTreeNode) *IntervalTree {
+    return &IntervalTree{node}
+}
+
+func (tree *IntervalTree) Insert(i *Interval) error {
+    tree.root = insertInterval(tree.root, i)
+    return nil
+}
+
+func (tree *IntervalTree) PrintTree() {
+    printNode(tree.root)
+}
+
+func (tree *IntervalTree) OverlapExists(i *Interval) bool {
+    return overlapExists(tree.root, i)
 }
 
 
 
+
+
+/*
+    A node in the interval tree, useful for balanced tree insertion
+    without having to contaminate interval class.
+ */
 type IntervalTreeNode struct {
     I *Interval
     Max time.Time
@@ -24,16 +48,68 @@ type IntervalTreeNode struct {
 }
 
 func NewIntervalTreeNode(i *Interval) *IntervalTreeNode {
-    inode := new(IntervalTreeNode)
-    inode.I = i
-    inode.Max = i.End
-    inode.Left = nil
-    inode.Right = nil
-    return inode
+    node := new(IntervalTreeNode)
+    node.I = i
+    node.Max = i.End
+    node.Left = nil
+    node.Right = nil
+    return node
+}
+
+func insertInterval(node *IntervalTreeNode, i *Interval) *IntervalTreeNode  {
+    // base case: create new node out of interval + return
+    if node == nil {
+        return NewIntervalTreeNode(i)
+    }
+
+    // recursive case (non null node): pass either left or right
+    start := node.I.Start
+    if i.Start.Before(start) {
+        node.Left = insertInterval(node.Left, i)
+    } else {
+        node.Right = insertInterval(node.Right, i)
+    }
+
+    // update max for searching later
+    if node.Max.Before(i.End) {
+        node.Max = i.End
+    }
+
+    return node
+}
+
+func printNode(node *IntervalTreeNode) {
+    if node == nil {
+        fmt.Println("end\n")
+    } else {
+        fmt.Println(*node.I)
+        fmt.Println("left")
+        printNode(node.Left)
+        fmt.Println("right")
+        printNode(node.Right)
+    }
+}
+
+func overlapExists(node *IntervalTreeNode, i *Interval) bool {
+    if node == nil {
+        return false
+    } else {
+        if Overlaps(*node.I, *i) {
+            return true
+        } else if overlapExists(node.Left, i) {
+            return true
+        } else {
+            return overlapExists(node.Right, i)
+        }
+    }
 }
 
 
 
+/*
+    Start and End times only. 
+    TODO: polymorphic intervals
+ */
 type Interval struct {
     Start time.Time
     End time.Time
@@ -47,4 +123,9 @@ func NewInterval(start time.Time, end time.Time) (*Interval, error) {
     } else {
         return &Interval{start, end}, nil
     }
+}
+
+// start_datetime1 <= end_datetime2 and end_datetime1 >= start_datetime2
+func Overlaps(i1 Interval, i2 Interval) bool {
+    return i1.Start.Before(i2.End) && i1.End.After(i2.Start)
 }
