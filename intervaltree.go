7410efb7ebeb6321d875a75f9d4034c7f5b2c05d
Nick Webb <njwebb@ucdavis.edu>
2018-03-18 18:14:04 -0700

    created structs for Interval, IntervalTree, IntervalTreeNode

--- intervaltree.go
+++ intervaltree.go
@@ -1,10 +1,50 @@
 package intervaltree
 
-// Reverse returns its argument string reversed rune-wise left to right.
-func Reverse(s string) string {
-    r := []rune(s)
-    for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
-        r[i], r[j] = r[j], r[i]
+import (
+    "time"
+    "errors"
+)
+
+
+type IntervalTree struct {
+    Root *IntervalTreeNode
+}
+
+func NewIntervalTree(inode *IntervalTreeNode) *IntervalTree {
+    return &IntervalTree{inode}
+}
+
+
+
+type IntervalTreeNode struct {
+    I *Interval
+    Max time.Time
+    Left *IntervalTreeNode
+    Right *IntervalTreeNode
+}
+
+func NewIntervalTreeNode(i *Interval) *IntervalTreeNode {
+    inode := new(IntervalTreeNode)
+    inode.I = i
+    inode.Max = i.End
+    inode.Left = nil
+    inode.Right = nil
+    return inode
+}
+
+
+
+type Interval struct {
+    Start time.Time
+    End time.Time
+}
+
+func NewInterval(start time.Time, end time.Time) (*Interval, error) {
+    if start.After(end) {
+        return nil, errors.New("interval: end cannot come after start.")
+    } else if start.Equal(end) {
+        return nil, errors.New("interval: start cannot equal end.")
+    } else {
+        return &Interval{start, end}, nil
     }
-    return string(r)
 }
